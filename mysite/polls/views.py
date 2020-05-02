4119680e5031115b072608da1249c236e08e1396
abditimer <thatimer@gmail.com>
2020-05-02 15:53:24 +0100

    pages done

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # The context is a dictionary mapping template variable names to Python objects.
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # The render() function takes the request object as its first argument, 
+    # a template name as its second argument and a dictionary as its optional third argument. 
+    # It returns an HttpResponse object of the given template rendered with the given context.
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    # displays a question text, with no results but with a form to vote.
+
+    # get_object_or_404, takes a model & keywords to pass to get() function to filter
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    # displays results for a particular question.
+    return HttpResponse(f"You are looking at the result of the question{question_id}")
+
+def vote(request, question_id):
+    # handles voting for a particular choice in a particular question.
+    return HttpResponse(f"You're voting on question {question_id}")
